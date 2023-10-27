fe407924feaa95fad7a5ffbfe0e6a750ccb48d90
redbulld007 <lixiang18333@163.com>
2023-10-27 15:50:08 +0800

    更换为了alaxnet模型，去除了评测流程，评测流程报错未修改

--- week2/yanjishibie.py
+++ week2/yanjishibie.py
@@ -221,51 +221,69 @@ from paddle.nn import Conv2D, MaxPool2D, Linear, Dropout
 import paddle.nn.functional as F
 
 # 定义 LeNet 网络结构
-class LeNet(paddle.nn.Layer):
-    def __init__(self, num_classes=1):
-        super(LeNet, self).__init__()
-        self.num_classes = num_classes
-        # 创建卷积和池化层块，每个卷积层使用Sigmoid激活函数，后面跟着一个2x2的池化
-        self.conv1 = Conv2D(in_channels=3, out_channels=6, kernel_size=5)
-        self.max_pool1 = MaxPool2D(kernel_size=2, stride=2)
-        self.conv2 = Conv2D(in_channels=6, out_channels=16, kernel_size=5)
-        self.max_pool2 = MaxPool2D(kernel_size=2, stride=2)
-        # 创建第3个卷积层
-        self.conv3 = Conv2D(in_channels=16, out_channels=120, kernel_size=4)
-        # 创建全连接层，第一个全连接层的输出神经元个数为64
-        self.fc1 = Linear(in_features=300000, out_features=64)
-        # 第二个全连接层输出神经元个数为分类标签的类别数
-        self.fc2 = Linear(in_features=64, out_features=num_classes)
+# -*- coding:utf-8 -*-
 
-    # 网络的前向计算过程
-    def forward(self, x, label=None):
+# 导入需要的包
+import paddle
+import numpy as np
+from paddle.nn import Conv2D, MaxPool2D, Linear, Dropout
+## 组网
+import paddle.nn.functional as F
+
+
+# 定义 AlexNet 网络结构
+class AlexNet(paddle.nn.Layer):
+    def __init__(self, num_classes=1):
+        super(AlexNet, self).__init__()
+        # AlexNet与LeNet一样也会同时使用卷积和池化层提取图像特征
+        # 与LeNet不同的是激活函数换成了‘relu’
+        self.conv1 = Conv2D(in_channels=3, out_channels=96, kernel_size=11, stride=4, padding=5)
+        self.max_pool1 = MaxPool2D(kernel_size=2, stride=2)
+        self.conv2 = Conv2D(in_channels=96, out_channels=256, kernel_size=5, stride=1, padding=2)
+        self.max_pool2 = MaxPool2D(kernel_size=2, stride=2)
+        self.conv3 = Conv2D(in_channels=256, out_channels=384, kernel_size=3, stride=1, padding=1)
+        self.conv4 = Conv2D(in_channels=384, out_channels=384, kernel_size=3, stride=1, padding=1)
+        self.conv5 = Conv2D(in_channels=384, out_channels=256, kernel_size=3, stride=1, padding=1)
+        self.max_pool5 = MaxPool2D(kernel_size=2, stride=2)
+
+        self.fc1 = Linear(in_features=12544, out_features=4096)
+        self.drop_ratio1 = 0.5
+        self.drop1 = Dropout(self.drop_ratio1)
+        self.fc2 = Linear(in_features=4096, out_features=4096)
+        self.drop_ratio2 = 0.5
+        self.drop2 = Dropout(self.drop_ratio2)
+        self.fc3 = Linear(in_features=4096, out_features=num_classes)
+
+    def forward(self, x):
         x = self.conv1(x)
-        x = F.sigmoid(x)
+        x = F.relu(x)
         x = self.max_pool1(x)
         x = self.conv2(x)
-        x = F.sigmoid(x)
+        x = F.relu(x)
         x = self.max_pool2(x)
         x = self.conv3(x)
-        x = F.sigmoid(x)
+        x = F.relu(x)
+        x = self.conv4(x)
+        x = F.relu(x)
+        x = self.conv5(x)
+        x = F.relu(x)
+        x = self.max_pool5(x)
         x = paddle.reshape(x, [x.shape[0], -1])
         x = self.fc1(x)
-        x = F.sigmoid(x)
+        x = F.relu(x)
+        # 在全连接之后使用dropout抑制过拟合
+        x = self.drop1(x)
         x = self.fc2(x)
-        if label is not None:
-            if self.num_classes == 1:
-                pred = F.sigmoid(x)
-                pred = paddle.concat([1.0 - pred, pred], axis=1)
-                acc = paddle.metric.accuracy(pred, paddle.cast(label, dtype='int64'))
-            else:
-                acc = paddle.metric.accuracy(x, paddle.cast(label, dtype='int64'))
-            return x, acc
-        else:
-            return x
+        x = F.relu(x)
+        # 在全连接之后使用dropout抑制过拟合
+        x = self.drop2(x)
+        x = self.fc3(x)
+        return x
 
 # 创建模型
-model = LeNet(num_classes=1)
+model = AlexNet()
 # 启动训练过程
-opt = paddle.optimizer.Momentum(learning_rate=0.001, momentum=0.9, parameters=model.parameters())
+opt = paddle.optimizer.Adam(learning_rate=0.001, parameters=model.parameters())
+
 train_pm(model, optimizer=opt)
-evaluation(model, params_file_path="palm.pdparams")
 
