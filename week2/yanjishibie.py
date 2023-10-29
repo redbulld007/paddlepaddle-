9fd6fb7ce797d88a7f5775eb154e112bda0feb24
redbulld007 <lixiang18333@163.com>
2023-10-30 07:29:20 +0800

    yolov3

--- week2/yanjishibie.py
+++ week2/yanjishibie.py
@@ -1,140 +1,262 @@
-# 图像分类，眼疾识别
-# 导入需要的包
-import paddle
+import cv2
+import random
 import numpy as np
-from paddle.nn import Conv2D, MaxPool2D, Linear
-
-## 组网
-import paddle.nn.functional as F
-
-# 定义 LeNet 网络结构
-class LeNet(paddle.nn.Layer):
-    def __init__(self, num_classes=1):
-        super(LeNet, self).__init__()
-        # 创建卷积和池化层
-        # 创建第1个卷积层
-        self.conv1 = Conv2D(in_channels=1, out_channels=6, kernel_size=5)
-        self.max_pool1 = MaxPool2D(kernel_size=2, stride=2)
-        # 尺寸的逻辑：池化层未改变通道数；当前通道数为6
-        # 创建第2个卷积层
-        self.conv2 = Conv2D(in_channels=6, out_channels=16, kernel_size=5)
-        self.max_pool2 = MaxPool2D(kernel_size=2, stride=2)
-        # 创建第3个卷积层
-        self.conv3 = Conv2D(in_channels=16, out_channels=120, kernel_size=4)
-        # 尺寸的逻辑：输入层将数据拉平[B,C,H,W] -> [B,C*H*W]
-        # 输入size是[28,28]，经过三次卷积和两次池化之后，C*H*W等于120
-        self.fc1 = Linear(in_features=120, out_features=64)
-        # 创建全连接层，第一个全连接层的输出神经元个数为64， 第二个全连接层输出神经元个数为分类标签的类别数
-        self.fc2 = Linear(in_features=64, out_features=num_classes)
-    # 网络的前向计算过程
-    def forward(self, x):
-        x = self.conv1(x)
-        # 每个卷积层使用Sigmoid激活函数，后面跟着一个2x2的池化
-        x = F.sigmoid(x)
-        x = self.max_pool1(x)
-        x = F.sigmoid(x)
-        x = self.conv2(x)
-        x = self.max_pool2(x)
-        x = self.conv3(x)
-        # 尺寸的逻辑：输入层将数据拉平[B,C,H,W] -> [B,C*H*W]
-        x = paddle.reshape(x, [x.shape[0], -1])
-        x = self.fc1(x)
-        x = F.sigmoid(x)
-        x = self.fc2(x)
-        return x
+import os
 
 
-# 输入数据形状是 [N, 1, H, W]
-# 这里用np.random创建一个随机数组作为输入数据
-x = np.random.randn(*[3,1,28,28])
-x = x.astype('float32')
+# 对读入的图像数据进行预处理
+def transform_img(img):
+    # 将图片尺寸缩放道 224x224
+    img = cv2.resize(img, (224, 224))
+    # 读入的图像数据格式是[H, W, C]
+    # 使用转置操作将其变成[C, H, W]
+    img = np.transpose(img, (2,0,1))
+    img = img.astype('float32')
+    # 将数据范围调整到[-1.0, 1.0]之间
+    img = img / 255.
+    img = img * 2.0 - 1.0
+    return img
 
-# 创建LeNet类的实例，指定模型名称和分类的类别数目
-model = LeNet(num_classes=10)
-# 通过调用LeNet从基类继承的sublayers()函数，
-# 查看LeNet中所包含的子层
-print(model.sublayers())
-x = paddle.to_tensor(x)
-for item in model.sublayers():
-    # item是LeNet类中的一个子层
-    # 查看经过子层之后的输出数据形状
-    try:
-        x = item(x)
-    except:
-        x = paddle.reshape(x, [x.shape[0], -1])
-        x = item(x)
-    if len(item.parameters())==2:
-        # 查看卷积和全连接层的数据和参数的形状，
-        # 其中item.parameters()[0]是权重参数w，item.parameters()[1]是偏置参数b
-        print(item.full_name(), x.shape, item.parameters()[0].shape, item.parameters()[1].shape)
-    else:
-        # 池化层没有参数
-        print(item.full_name(), x.shape)
+# 定义训练集数据读取器
+def data_loader(datadir, batch_size=10, mode = 'train'):
+    # 将datadir目录下的文件列出来，每条文件都要读入
+    filenames = os.listdir(datadir)
+
+    def reader():
+        if mode == 'train':
+            # 训练时随机打乱数据顺序
+            random.shuffle(filenames)
+        batch_imgs = []
+        batch_labels = []
+        for name in filenames:
+            filepath = os.path.join(datadir, name)
+            img = cv2.imread(filepath)
+            img = transform_img(img)
+            if name[0] == 'H' or name[0] == 'N':
+                # H开头的文件名表示高度近似，N开头的文件名表示正常视力
+                # 高度近视和正常视力的样本，都不是病理性的，属于负样本，标签为0
+                label = 0
+            elif name[0] == 'P':
+                # P开头的是病理性近视，属于正样本，标签为1
+                label = 1
+            else:
+                raise('Not excepted file name')
+            # 每读取一个样本的数据，就将其放入数据列表中
+            batch_imgs.append(img)
+            batch_labels.append(label)
+            if len(batch_imgs) == batch_size:
+                # 当数据列表的长度等于batch_size的时候，
+                # 把这些数据当作一个mini-batch，并作为数据生成器的一个输出
+                imgs_array = np.array(batch_imgs).astype('float32')
+                labels_array = np.array(batch_labels).astype('float32').reshape(-1, 1)
+                yield imgs_array, labels_array
+                batch_imgs = []
+                batch_labels = []
+
+        if len(batch_imgs) > 0:
+            # 剩余样本数目不足一个batch_size的数据，一起打包成一个mini-batch
+            imgs_array = np.array(batch_imgs).astype('float32')
+            labels_array = np.array(batch_labels).astype('float32').reshape(-1, 1)
+            yield imgs_array, labels_array
+
+
+    return reader
+
+# 定义验证集数据读取器
+def valid_data_loader(datadir, csvfile, batch_size=10, mode='valid'):
+    # 训练集读取时通过文件名来确定样本标签，验证集则通过csvfile来读取每个图片对应的标签
+    # 请查看解压后的验证集标签数据，观察csvfile文件里面所包含的内容
+    # csvfile文件所包含的内容格式如下，每一行代表一个样本，
+    # 其中第一列是图片id，第二列是文件名，第三列是图片标签，
+    # 第四列和第五列是Fovea的坐标，与分类任务无关
+    # ID,imgName,Label,Fovea_X,Fovea_Y
+    # 1,V0001.jpg,0,1157.74,1019.87
+    # 2,V0002.jpg,1,1285.82,1080.47
+    # 打开包含验证集标签的csvfile，并读入其中的内容
+    filelists = open(csvfile).readlines()
+    def reader():
+        batch_imgs = []
+        batch_labels = []
+        for line in filelists[1:]:
+            line = line.strip().split(',')
+            name = line[1]
+            label = int(line[2])
+            # 根据图片文件名加载图片，并对图像数据作预处理
+            filepath = os.path.join(datadir, name)
+            img = cv2.imread(filepath)
+            img = transform_img(img)
+            # 每读取一个样本的数据，就将其放入数据列表中
+            batch_imgs.append(img)
+            batch_labels.append(label)
+            if len(batch_imgs) == batch_size:
+                # 当数据列表的长度等于batch_size的时候，
+                # 把这些数据当作一个mini-batch，并作为数据生成器的一个输出
+                imgs_array = np.array(batch_imgs).astype('float32')
+                labels_array = np.array(batch_labels).astype('float32').reshape(-1, 1)
+                yield imgs_array, labels_array
+                batch_imgs = []
+                batch_labels = []
+
+        if len(batch_imgs) > 0:
+            # 剩余样本数目不足一个batch_size的数据，一起打包成一个mini-batch
+            imgs_array = np.array(batch_imgs).astype('float32')
+            labels_array = np.array(batch_labels).astype('float32').reshape(-1, 1)
+            yield imgs_array, labels_array
+
+    return reader
 
 # -*- coding: utf-8 -*-
-# LeNet 识别手写数字
+# LeNet 识别眼疾图片
 import os
 import random
 import paddle
 import numpy as np
-import paddle
-from paddle.vision.transforms import ToTensor
-from paddle.vision.datasets import MNIST
+
+DATADIR = './data/PALM-Training400/PALM-Training400'
+DATADIR2 = './data/PALM-Validation400'
+CSVFILE = './data/labels.csv'
+# 设置迭代轮数
+EPOCH_NUM = 5
 
 # 定义训练过程
-def train(model, opt, train_loader, valid_loader):
+def train_pm(model, optimizer):
     # 开启0号GPU训练
     use_gpu = True
     paddle.device.set_device('gpu:0') if use_gpu else paddle.device.set_device('cpu')
+
     print('start training ... ')
     model.train()
+    # 定义数据读取器，训练数据读取器和验证数据读取器
+    train_loader = data_loader(DATADIR, batch_size=10, mode='train')
+    valid_loader = valid_data_loader(DATADIR2, CSVFILE)
     for epoch in range(EPOCH_NUM):
         for batch_id, data in enumerate(train_loader()):
-            img = data[0]
-            label = data[1]
-            # 计算模型输出
+            x_data, y_data = data
+            img = paddle.to_tensor(x_data)
+            label = paddle.to_tensor(y_data)
+            # 运行模型前向计算，得到预测值
             logits = model(img)
-            # 计算损失函数
-            loss_func = paddle.nn.CrossEntropyLoss(reduction='none')
-            loss = loss_func(logits, label)
+            loss = F.binary_cross_entropy_with_logits(logits, label)
             avg_loss = paddle.mean(loss)
 
-            if batch_id % 2000 == 0:
+            if batch_id % 20 == 0:
                 print("epoch: {}, batch_id: {}, loss is: {:.4f}".format(epoch, batch_id, float(avg_loss.numpy())))
+            # 反向传播，更新权重，清除梯度
             avg_loss.backward()
-            opt.step()
-            opt.clear_grad()
+            optimizer.step()
+            optimizer.clear_grad()
 
         model.eval()
         accuracies = []
         losses = []
         for batch_id, data in enumerate(valid_loader()):
-            img = data[0]
-            label = data[1]
-            # 计算模型输出
+            x_data, y_data = data
+            img = paddle.to_tensor(x_data)
+            label = paddle.to_tensor(y_data)
+            # 运行模型前向计算，得到预测值
             logits = model(img)
-            pred = F.softmax(logits)
-            # 计算损失函数
-            loss_func = paddle.nn.CrossEntropyLoss(reduction='none')
-            loss = loss_func(logits, label)
-            acc = paddle.metric.accuracy(pred, label)
+            # 二分类，sigmoid计算后的结果以0.5为阈值分两个类别
+            # 计算sigmoid后的预测概率，进行loss计算
+            pred = F.sigmoid(logits)
+            loss = F.binary_cross_entropy_with_logits(logits, label)
+            # 计算预测概率小于0.5的类别
+            pred2 = pred * (-1.0) + 1.0
+            # 得到两个类别的预测概率，并沿第一个维度级联
+            pred = paddle.concat([pred2, pred], axis=1)
+            acc = paddle.metric.accuracy(pred, paddle.cast(label, dtype='int64'))
+
             accuracies.append(acc.numpy())
             losses.append(loss.numpy())
         print("[validation] accuracy/loss: {:.4f}/{:.4f}".format(np.mean(accuracies), np.mean(losses)))
         model.train()
 
-    # 保存模型参数
-    paddle.save(model.state_dict(), 'mnist.pdparams')
+        paddle.save(model.state_dict(), 'palm.pdparams')
+        paddle.save(optimizer.state_dict(), 'palm.pdopt')
+
+# 定义评估过程
+def evaluation(model, params_file_path):
+
+    # 开启0号GPU预估
+    use_gpu = True
+    paddle.device.set_device('gpu:0') if use_gpu else paddle.device.set_device('cpu')
+
+    print('start evaluation .......')
+
+    #加载模型参数
+    model_state_dict = paddle.load(params_file_path)
+    model.load_dict(model_state_dict)
+
+    model.eval()
+    eval_loader = data_loader(DATADIR,
+                        batch_size=10, mode='eval')
+
+    acc_set = []
+    avg_loss_set = []
+    for batch_id, data in enumerate(eval_loader()):
+        x_data, y_data = data
+        img = paddle.to_tensor(x_data)
+        label = paddle.to_tensor(y_data)
+        y_data = y_data.astype(np.int64)
+        label_64 = paddle.to_tensor(y_data)
+        # 计算预测和精度
+        prediction, acc = model(img, label_64)
+        # 计算损失函数值
+        loss = F.binary_cross_entropy_with_logits(prediction, label)
+        avg_loss = paddle.mean(loss)
+        acc_set.append(float(acc.numpy()))
+        avg_loss_set.append(float(avg_loss.numpy()))
+    # 求平均精度
+    acc_val_mean = np.array(acc_set).mean()
+    avg_loss_val_mean = np.array(avg_loss_set).mean()
+
+    print('loss={:.4f}, acc={:.4f}'.format(avg_loss_val_mean, acc_val_mean))
 
 
-# 创建模型
-model = LeNet(num_classes=10)
-# 设置迭代轮数
-EPOCH_NUM = 5
-# 设置优化器为Momentum，学习率为0.001
-opt = paddle.optimizer.Momentum(learning_rate=0.001, momentum=0.9, parameters=model.parameters())
-# 定义数据读取器
-train_loader = paddle.io.DataLoader(MNIST(mode='train', transform=ToTensor()), batch_size=10, shuffle=True)
-valid_loader = paddle.io.DataLoader(MNIST(mode='test', transform=ToTensor()), batch_size=10)
-# 启动训练过程
-train(model, opt, train_loader, valid_loader)
+import paddle
+from paddle.vision.models import resnet50
+
+# 调用高层API的resnet50模型
+model = resnet50()
+# 设置pretrained参数为True，可以加载resnet50在imagenet数据集上的预训练模型
+# model = resnet50(pretrained=True)
+
+# 随机生成一个输入
+x = paddle.rand([1, 3, 224, 224])
+# 得到残差50的计算结果
+out = model(x)
+# 打印输出的形状，由于resnet50默认的是1000分类
+# 所以输出shape是[1x1000]
+print(out.shape)
+
+# 从paddle.vision.models 模块中import 残差网络，VGG网络，LeNet网络
+from paddle.vision.models import resnet50, vgg16, LeNet
+from paddle.vision.datasets import Cifar10
+from paddle.optimizer import Momentum
+from paddle.regularizer import L2Decay
+from paddle.nn import CrossEntropyLoss
+from paddle.metric import Accuracy
+from paddle.vision.transforms import Transpose
+
+# 确保从paddle.vision.datasets.Cifar10中加载的图像数据是np.ndarray类型
+paddle.vision.set_image_backend('cv2')
+# 调用resnet50模型
+model = paddle.Model(resnet50(pretrained=False, num_classes=10))
+
+# 使用Cifar10数据集
+train_dataset = Cifar10(mode='train', transform=Transpose())
+val_dataset = Cifar10(mode='test', transform=Transpose())
+# 定义优化器
+optimizer = Momentum(learning_rate=0.01,
+                     momentum=0.9,
+                     weight_decay=L2Decay(1e-4),
+                     parameters=model.parameters())
+# 进行训练前准备
+model.prepare(optimizer, CrossEntropyLoss(), Accuracy(topk=(1, 5)))
+# 启动训练
+model.fit(train_dataset,
+          val_dataset,
+          epochs=10,
+          batch_size=64,
+          save_dir="./output",
+          num_workers=8)
+
